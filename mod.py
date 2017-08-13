9574b6dbdc620b027a31960853448c8aecc682a3
pradeep0492 <pradeep.gupta@mantralabsglobal.com>
2017-08-13 20:03:19 +0530

    save the code

---
+++ mod.py
@@ -0,0 +1,9 @@
+ def mod_result(x,n,d):
+        result=1
+        while (n > 0):
+            if (n % 2 == 1):
+                result = (result * x) % d
+            n = n >> 1
+            x = (x * x) % d
+        return result;
+mod_result(5,117,19)
